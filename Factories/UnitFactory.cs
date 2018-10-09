fe03606891ad5f449aa76fb1aad9dd0098ba39da
nhamed <hamed.nidhal@yahoo.fr>
2018-10-09 18:29:41 +0200

    evolution unitfactory

--- Factories/UnitFactory.cs
+++ Factories/UnitFactory.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BotFactory.Interface;
 using BotFactory.Models;
+using BotFactory.Common.Tools;
 
 namespace BotFactory.Factories
 {
@@ -13,6 +14,7 @@ namespace BotFactory.Factories
 
         private int QueueCapacity { get; set; }
         private int StorageCapacity { get; set; }
+        public Queue<FactoryQueueElement> Queue { get; set; }
         public UnitFactory() { }
         public UnitFactory(int taillequeue, int tailleentreprise)
         {
@@ -23,6 +25,26 @@ namespace BotFactory.Factories
             this.StorageCapacity = tailleentreprise;
         }
 
+        public bool AddWorkableUnitToQueue(string nom, Type model, Coordinates parkingPos, Coordinates workingPos)
+        {
+
+            {
+                if ((Queue.Count <= this.QueueCapacity) && ((Queue.Count <= this.StorageCapacity)))
+                    {
+                    FactoryQueueElement add = new FactoryQueueElement(nom, model, parkingPos, workingPos);
+                }
+            }
+            //- L’usine ne peut construire qu’un robot à la fois. 
+            //- L’usine ne peut enregistrer plus de commandes si sa queue est pleine 
+            //- L’usine ne peut construire plus de robots si son entrepôt est plein 
+            //- On peut appeler l’ajout de commande n’importe quand 
+            //- La méthode doit retourner false si la commande n’est pas enregistrée 
+            //- La construction doit être active tant que la queue n’est pas vide ou l’entrepôt plein 
+            //- La construction d’un robot doit être simulée et prendre le temps indiqué par la propriété BuildTime du robot 
+
+
+            return true;
+        }
 
 
     }
